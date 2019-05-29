7a71603ab8ef6f2e5b24898acf2957ce81c09e03
Kondász Ferenc <kondaszferenc@Kondaszs-MacBook-Pro.local>
2019-05-29 10:36:21 +0200

    pillow tests

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ ADD . /app
 
 RUN pip install --no-cache-dir -e .
 RUN pip install --no-cache-dir -r $requirements
-RUN apt -y update && apt -y upgrade && apt -y install libvips
+RUN apt -y update && apt -y upgrade && apt -y install libvips && apt-get install libpng-dev && apt-get install zlib1g-dev
+RUN CC="cc -mavx2" pip install -U --force-reinstall pillow-simd
--- skeleton/routes.py
+++ skeleton/routes.py
@@ -1,16 +1,20 @@
 from subprocess import Popen
+import io
 import os
 import subprocess
 import pathlib
 import pyvips
 
 from aiohttp import web
+from PIL import Image
+
 from skeleton.main.views import index
+
 
 PROJECT_PATH = pathlib.Path(__file__).parent
 
 
-async def cmd(request):
+async def vips(request):
 
     request_data = await request.post()
     in_cmd = request_data['cmd']
@@ -40,19 +44,42 @@ async def cmd(request):
         image.resize(0.5)
         saved_file_name = '{}-{}.jpg'.format('medium-resized', i)
 
-    # if in_cmd == 'tiff_conv':
-    #     image = pyvips.Image.tiffload(
-    #         '/images/reads/test.tiff', access='sequential')
-    #     saved_file_name = '{}-{}.jpg'.format('tiff-converted', i)
-
-    # if in_cmd == 'tiff_rotate':
-    #     image = pyvips.Image.tiffload(
-    #         '/images/reads/test.tiff', access='sequential')
-    #     image.rot90()
-    #     saved_file_name = '{}-{}.tiff'.format('tiff-rotated', i)
-
     if saved_file_name:
         image.write_to_file('/images/writes/{}'.format(saved_file_name))
+
+    return web.Response(text='{} is done\n'.format(saved_file_name))
+
+
+async def pillow(request):
+    request_data = await request.post()
+    in_cmd = request_data['cmd']
+    i = request_data['i']
+
+    saved_file_name = ""
+
+    if in_cmd == 'large_conv':
+        image = Image.open('/images/reads/large.jpg', mode='r')
+        saved_file_name = '{}-{}.png'.format('large-pillow-converted', i)
+        image.save('/images/writes/{}'.format(saved_file_name), 'PNG')
+
+    if in_cmd == 'large_resize':
+        image = Image.open('/images/reads/large.jpg', mode='r')
+        image.resize(tuple(int(n/2)
+                           for n in image.size), resample=Image.LANCZOS)
+        saved_file_name = '{}-{}.jpg'.format('large-pillow-resized', i)
+        image.save('/images/writes/{}'.format(saved_file_name), 'JPEG')
+
+    if in_cmd == 'medium_conv':
+        image = Image.open('/images/reads/medium.jpg', mode='r')
+        saved_file_name = '{}-{}.png'.format('medium-pillow-converted', i)
+        image.save('/images/writes/{}'.format(saved_file_name), 'PNG')
+
+    if in_cmd == 'medium_resize':
+        image = Image.open('/images/reads/medium.jpg', mode='r')
+        image.resize(tuple(int(n/2)
+                           for n in image.size), resample=Image.LANCZOS)
+        saved_file_name = '{}-{}.jpg'.format('medium-pillow-resized', i)
+        image.save('/images/writes/{}'.format(saved_file_name), 'JPEG')
 
     return web.Response(text='{} is done\n'.format(saved_file_name))
 
@@ -60,7 +87,8 @@ async def cmd(request):
 def init_routes(app: web.Application) -> None:
 
     app.router.add_route('*', '/', index, name='index')
-    app.add_routes([web.post('/cmd', cmd)])
+    app.add_routes([web.post('/vips', vips)])
+    app.add_routes([web.post('/pillow', pillow)])
 
     # added static dir
     app.router.add_static(
